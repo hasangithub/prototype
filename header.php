be9649e1bf1c519c341d963d7292e2526010a7c9
hasan <hasanbannah@gmail.com>
2024-09-13 20:38:51 +0530

    changes bs5 3

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <!-- header.php -->
 <header class="navbar navbar-dark bg-dark sticky-top flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Admin Dashboard</a>
+    <button class="navbar-toggler d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="navbar-nav">
         <div class="nav-item text-nowrap">
             <a class="nav-link px-3" href="#">Sign out</a>
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <!-- sidebar.php -->
-<nav class="col-md-3 col-lg-2 d-md-block bg-dark sidebar">
+<nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
     <div class="position-sticky pt-3">
         <ul class="nav flex-column">
             <li class="nav-item">
